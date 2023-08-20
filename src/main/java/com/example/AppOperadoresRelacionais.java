a398ee65b4458e42eab74e49a1edd392c722f391
Diego Vale <physicist.dev@gmail.com>
2023-08-20 23:20:58 +0000

    adicionado AppOperadoresRelacionais.java

---
+++ src/main/java/com/example/AppOperadoresRelacionais.java
@@ -0,0 +1,20 @@
+package com.example;
+
+public class AppOperadoresRelacionais {
+    public static void main(String[] args) {
+        var cliente1 = new Cliente();
+        cliente1.setAnoNascimento(1980);
+        
+        var cliente2 = new Cliente();
+        cliente2.setAnoNascimento(1981);
+
+        if(cliente1.getAnoNascimento() == cliente2.getAnoNascimento()){
+            System.out.println("Os clientes nasceram no mesmo ano.");
+            System.out.println("O ano de nascimento dos clientes é "+cliente1.getAnoNascimento());
+        }else{
+            System.out.println("Os clientes nasceram em anos diferentes.");
+            System.out.println("O ano de nascimento do cliente 1 é "+cliente1.getAnoNascimento());
+            System.out.println("O ano de nascimento do cliente 2 é "+cliente2.getAnoNascimento());
+        }
+    }
+}
